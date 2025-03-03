d7200f6a148538f0fe54c352e8dc3d6d450f862d
torres-jp <torres.jpw@gmail.com>
2025-03-02 21:32:08 -0300

    add schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,15 @@ import { defineCollection, z } from 'astro:content'
 const books = defineCollection({
   schema: z.object({
     title: z.string(),
+    author: z.string(),
     description: z.string(),
     image: z.string(),
+    readtime: z.number(),
+    buy: z.object({
+      spain: z.string().url(),
+      usa: z.string().url(),
+    }),
   }),
 })
+
+export const collections = { books }
