a3cf4fe50d151823056b1233f92d9933b1200801
torres-jp <torres.jpw@gmail.com>
2025-03-05 14:12:41 -0300

    end project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
-// @ts-check
 import { defineConfig, envField } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()],
   },
   env: {
     schema: {
-      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
-    }
-  }
+      SHOW_BUY_BUTTON: envField.boolean({
+        default: true,
+        context: 'server',
+        access: 'public',
+      }),
+      SCORE_API_ENDPOINT: envField.string({
+        context: 'server',
+        access: 'public',
+      }),
+    },
+  },
 })
